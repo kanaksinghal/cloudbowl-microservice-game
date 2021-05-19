a2ff5e2a2c7658b91dc3cda0b11964976825d599
Kanak Singhal <kanak@cldcvr.com>
2021-05-20 01:57:39 +0530

    my algo

--- samples/nodejs/web.js
+++ samples/nodejs/web.js
@@ -4,10 +4,99 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
-  console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+let manualCMD = '';
+let myDir = '';
+let nextDir = '';
+
+app.all('/*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 });
 
+app.post('/', function (req, res) {
+	if(manualCMD) {
+		res.send(manualCMD);
+		myDir = nextDir;
+		manualCMD = '';
+	} else {
+		res.send('T');
+		myDir = req.body.arena.state[req.body._links.self.href].direction
+	}
+});
+
+app.post('/set', function (req, res) {
+	manualCMD = udlrToFLR(req.body.move)
+	console.log(req.body, myDir, manualCMD, nextDir);
+	res.send(`Saved ${manualCMD}`);
+});
+
+function udlrToFLR(arrow) {
+	switch (myDir) {
+		case 'N':
+			switch (arrow) {
+				case 'U':
+					nextDir = 'N';
+					return 'F';
+				case 'D':
+				case 'R':
+					nextDir = 'E';
+					return 'R';
+				case 'L':
+					nextDir = 'W';
+					return 'L';
+			}
+			break;
+		case 'E':
+			switch (arrow) {
+				case 'R':
+					nextDir = 'E';
+					return 'F';
+				case 'L':
+				case 'D':
+					nextDir = 'S';
+					return 'R';
+				case 'U':
+					nextDir = 'N';
+					return 'L';
+			}
+			break;
+		case 'S':
+			switch (arrow) {
+				case 'D':
+					nextDir = 'S';
+					return 'F';
+				case 'L':
+				case 'U':
+					nextDir = 'W';
+					return 'R';
+				case 'R':
+					nextDir = 'E';
+					return 'L';
+			}
+			break;
+		case 'W':
+			switch (arrow) {
+				case 'L':
+					nextDir = 'W';
+					return 'F';
+				case 'U':
+				case 'R':
+					nextDir = 'N';
+					return 'R';
+				case 'D':
+					nextDir = 'S';
+					return 'L';
+			}
+			break;
+	}
+	return ''
+}
+
 app.listen(process.env.PORT || 8080);
+
+//   N
+// W   E
+//   S
+
